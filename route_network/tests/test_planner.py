d95c90a97c550d737f7c04d4674f287c6a8754fc
jack.oquin <jack.oquin@f2b30410-025d-11df-9f77-c71dee008b03>
2012-05-11 22:34:28 +0000

    add test with multiple route segments

--- route_network/tests/test_planner.py
+++ route_network/tests/test_planner.py
@@ -72,6 +72,48 @@ def tiny_oneway():
 
     return r
 
+def triangle_graph():
+    'initialize test data: three points with one-way segments between them'
+    r = RouteNetwork()
+
+    w = WayPoint()
+    w.id = UniqueID(uuid='da7c242f-2efe-5175-9961-49cc621b80b9')
+    w.position = GeoPoint(latitude=30.3840168, longitude=-97.72821)
+    r.points.append(w)
+
+    w = WayPoint()
+    w.id = UniqueID(uuid='812f1c08-a34b-5a21-92b9-18b2b0cf4950')
+    w.position = GeoPoint(latitude=30.385729, longitude=-97.7316754)
+    r.points.append(w)
+
+    w = WayPoint()
+    w.id = UniqueID(uuid='2b093523-3b39-5c48-a11c-f7c65650c581')
+    w.position = GeoPoint(latitude=30.3849643, longitude=-97.7269564)
+    r.points.append(w)
+
+    s = RouteSegment()
+    s.id = UniqueID(uuid='41b7d2be-3c37-5a78-a7ac-248d939af379')
+    s.start = UniqueID(uuid='da7c242f-2efe-5175-9961-49cc621b80b9')
+    s.end = UniqueID(uuid='812f1c08-a34b-5a21-92b9-18b2b0cf4950')
+    geodesy.props.put(s, 'oneway', 'yes')
+    r.segments.append(s)
+
+    s = RouteSegment()
+    s.id = UniqueID(uuid='2df38f2c-202b-5ba5-be73-c6498cb4aafe')
+    s.start = UniqueID(uuid='812f1c08-a34b-5a21-92b9-18b2b0cf4950')
+    s.end = UniqueID(uuid='2b093523-3b39-5c48-a11c-f7c65650c581')
+    geodesy.props.put(s, 'oneway', 'yes')
+    r.segments.append(s)
+
+    s = RouteSegment()
+    s.id = UniqueID(uuid='8f2c2df3-be73-5ba5-202b-cb4aafec6498')
+    s.start = UniqueID(uuid='2b093523-3b39-5c48-a11c-f7c65650c581')
+    s.end = UniqueID(uuid='da7c242f-2efe-5175-9961-49cc621b80b9')
+    geodesy.props.put(s, 'oneway', 'yes')
+    r.segments.append(s)
+
+    return r
+
 class TestPlanner(unittest.TestCase):
     """Unit tests for planner module.
     """
@@ -169,6 +211,29 @@ class TestPlanner(unittest.TestCase):
                                   start='812f1c08-a34b-5a21-92b9-18b2b0cf4950',
                                   goal='deadbeef') # invalid way point
         self.assertRaises(ValueError, pl.planner, req)
+
+    def test_triangle_routes(self):
+        pl = Planner(triangle_graph())
+
+        # going forward should yield a single route segment
+        req = GetRoutePlanRequest(network=pl.graph.id,
+                                  start='da7c242f-2efe-5175-9961-49cc621b80b9',
+                                  goal='812f1c08-a34b-5a21-92b9-18b2b0cf4950')
+        path = pl.planner(req)
+        self.assertEqual(len(path.segments), 1)
+        self.assertEqual(path.segments[0].uuid,
+                         '41b7d2be-3c37-5a78-a7ac-248d939af379')
+
+        # going backward should yield two route segments
+        req = GetRoutePlanRequest(network=pl.graph.id,
+                                  start='812f1c08-a34b-5a21-92b9-18b2b0cf4950',
+                                  goal='da7c242f-2efe-5175-9961-49cc621b80b9')
+        path = pl.planner(req)
+        self.assertEqual(len(path.segments), 2)
+        self.assertEqual(path.segments[0].uuid,
+                         '2df38f2c-202b-5ba5-be73-c6498cb4aafe')
+        self.assertEqual(path.segments[1].uuid,
+                         '8f2c2df3-be73-5ba5-202b-cb4aafec6498')
         
 
 if __name__ == '__main__':
