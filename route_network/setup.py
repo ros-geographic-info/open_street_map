55bd67f5630bc5893413b1466c2afa01bb8fd7a6
Jack O'Quin <jack.oquin@gmail.com>
2013-09-17 19:16:52 -0500

    test_osm: initial conversion of to catkin (#2)

--- route_network/setup.py
+++ route_network/setup.py
@@ -4,13 +4,14 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['osm_cartography'],
+    packages=['route_network'],
     package_dir={'': 'src'},
     install_requires=['rospy',
                       'geodesy',
                       'geographic_msgs',
                       'geometry_msgs',
-                      'std_msgs',
+                      'nav_msgs',
+                      'rospy',
                       'visualization_msgs'],
     )
 
